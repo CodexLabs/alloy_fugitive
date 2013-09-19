b44ebdf3964e9da94ba00e403488171e4834f0a0
Andrew McElroy <sophrinix@gmail.com>
2013-09-19 00:19:01 -0500

    deleted:    runios.sh 	deleted:    ticucumber-ios.sh 	modified:   tiwificucumber.sh 	run.sh 	ticucumber.sh

--- tiwificucumber.sh
+++ tiwificucumber.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd build/iphone
-DEVICE_ENDPOINT=http://192.168.1.124:37265 cucumber
+echo 'ignore me for now. I have noe fleshed this poc out enough yet'
+#cd build/iphone
+#DEVICE_ENDPOINT=http://0.0.0.0:37265 cucumber
